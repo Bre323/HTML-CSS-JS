9cd08a1458d1c91657df307f84fcf417744b4a45
Breno Rocha <51891048+Bre323@users.noreply.github.com>
2025-04-25 11:37:46 -0300

    PlayRound function

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,4 +1,5 @@
 const options = ['Rock', 'Paper', 'Scissors'];
+let [computerScore, humanScore] = [0, 0];
 
 function getComputerChoice() {
     let index = Math.floor(Math.random() * 3);
@@ -13,4 +14,27 @@ function getHumanChoice() {
          3 - Scissors
     `));
     return options[choice - 1];
+}
+
+function playRound(humanChoice, computerChoice) {
+    let humanValue = options[humanChoice];
+    let computerValue = options[computerChoice];
+
+    if (
+        (humanValue === 'Rock' && computerValue === 'Scissors') ||
+        (humanValue === 'Paper' && computerValue === 'Rock') ||
+        (humanValue === 'Scissors' && computerValue === 'Paper')
+    ) {
+        console.log(`You Win! ${humanValue} beats ${computerValue}`);
+        humanScore++;
+    }
+
+    else if (
+        (computerValue === 'Rock' && humanValue === 'Scissors') ||
+        (computerValue === 'Paper' && humanValue === 'Rock') ||
+        (computerValue === 'Scissors' && humanValue === 'Paper')
+    ) {
+        console.log(`You Lose! ${computerValue} beats ${humanValue}`);
+        computerScore++;
+    }
 }
