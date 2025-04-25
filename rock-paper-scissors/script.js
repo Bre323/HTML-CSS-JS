e76cfcbc506ca87e3a7cdc6179bc3cd79f56fb37
Breno Rocha <51891048+Bre323@users.noreply.github.com>
2025-04-25 12:37:30 -0300

    Finished game

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,40 +1,79 @@
-const options = ['Rock', 'Paper', 'Scissors'];
-let [computerScore, humanScore] = [0, 0];
+function playGame() {
+    const options = ['Rock', 'Paper', 'Scissors'];
+    let [computerScore, humanScore] = [0, 0];
 
-function getComputerChoice() {
-    let index = Math.floor(Math.random() * 3);
-    return options[index];
-}
-
-function getHumanChoice() {
-    let choice = Number(prompt(`
-        Choose an option: 
-         1 - Rock
-         2 - Paper
-         3 - Scissors
-    `));
-    return options[choice - 1];
-}
-
-function playRound(humanChoice, computerChoice) {
-    let humanValue = options[humanChoice];
-    let computerValue = options[computerChoice];
-
-    if (
-        (humanValue === 'Rock' && computerValue === 'Scissors') ||
-        (humanValue === 'Paper' && computerValue === 'Rock') ||
-        (humanValue === 'Scissors' && computerValue === 'Paper')
-    ) {
-        console.log(`You Win! ${humanValue} beats ${computerValue}`);
-        humanScore++;
+    
+    function getComputerChoice() {
+        let index = Math.floor(Math.random() * 3);
+        return options[index];
     }
 
-    else if (
-        (computerValue === 'Rock' && humanValue === 'Scissors') ||
-        (computerValue === 'Paper' && humanValue === 'Rock') ||
-        (computerValue === 'Scissors' && humanValue === 'Paper')
-    ) {
-        console.log(`You Lose! ${computerValue} beats ${humanValue}`);
-        computerScore++;
+    function getHumanChoice() {
+        let choice = Number(prompt(`
+            Choose an option: 
+            1 - Rock
+            2 - Paper
+            3 - Scissors
+        `));
+        return options[choice - 1];
+    }
+
+    function playRound(humanChoice) {
+        let computerChoice = getComputerChoice();
+    
+        if (
+            (humanChoice === 'Rock' && computerChoice === 'Scissors') ||
+            (humanChoice === 'Paper' && computerChoice === 'Rock') ||
+            (humanChoice === 'Scissors' && computerChoice === 'Paper')
+        ) {
+            alert(`You Win! ${humanChoice} beats ${computerChoice}`);
+            humanScore++;
+            console.log(`Your Score: ${humanScore}`);
+        }
+    
+        else if (
+            (computerChoice === 'Rock' && humanChoice === 'Scissors') ||
+            (computerChoice === 'Paper' && humanChoice === 'Rock') ||
+            (computerChoice === 'Scissors' && humanChoice === 'Paper')
+        ) {
+            alert(`You Lose! ${computerChoice} beats ${humanChoice}`);
+            computerScore++;
+            console.log(`Computer Score: ${computerScore}`);
+        }
+    
+        else {
+            alert('Tie!');
+        }
+    }
+
+
+    while (humanScore < 3 && computerScore < 3) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+
+    if(humanScore === 3) {
+        alert(`
+            YOU WON THE GAME!!!
+
+            Your Score: ${humanScore}
+            Computer Score: ${computerScore}
+        `);
+
+        humanScore = 0;
+        computerScore = 0;
+        return;
+    }
+
+    if(computerScore === 3) {
+        alert(`
+            YOU LOST THE GAME!!!
+            
+            Your Score: ${humanScore}
+            Computer Score: ${computerScore}
+        `);
+
+        humanScore = 0;
+        computerScore = 0;
+        return;
     }
 }
